1cab0785c67b32f1d2d8e403f26765122a45e85d
user <msazad202@gmail.com>
2023-11-08 22:45:38 +0530

    first step

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"github.com/google/uuid"
+)
+
+var tmpl *template.Template
+
+var dbSessions = make(map[string]string)
+var dbUsers = make(map[string]user)
+
+type user struct {
+	Name     string
+	Username string
+	Password string
+}
+type errors struct {
+	UserError string
+	PassError string
+	FullError string
+}
+
+var errorval errors
+
+func init() {
+	template.Must(template.ParseGlob())
+	dbUsers["azad@gmail.com"]=user{"azad","azad@gmail.com","1212"}
+}
+func main() {
+	fmt.Println("Server is running in port :8080")
+	http.HandleFunc("/", loginHandler)
+}
+func loginHandler(w http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("session")
+	if err == nil {
+		if _, ok := dbSessions[cookie.Value]; ok {
+			http.Redirect(w, req, "/home", http.StatusSeeOther)
+		}
+	}
+	if req.Method == http.MethodPost {
+		uname := req.FormValue("username")
+		pass := req.FormValue("password")
+		// check username
+		if _, ok := dbUsers[uname]; !ok {
+			http.Redirect(w, req, "/", http.StatusSeeOther)
+			errorval.UserError = "Username Error"
+			return
+		}
+		//check Password
+		if pass != dbUsers[uname].Password {
+			http.Redirect(w, req, "/", http.StatusSeeOther)
+			errorval.PassError = "Password Error"
+			return
+		}
+		//if Password matches
+		if pass==dbUsers[uname].Password{
+			//Create Cookie
+		}
+	}
+}
